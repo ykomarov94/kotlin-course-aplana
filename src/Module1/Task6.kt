b7f6b71cae8045d6de6b26aeab246c2674b2b56e
ykomarov94 <ykomarov94@gmail.com>
2019-10-06 15:11:18 +0300

    Задание 6: Сравнение

---
+++ src/Module1/Task6.kt
@@ -0,0 +1,11 @@
+package Module1
+
+fun main() {
+    println("input a")
+    val a = readLine()!!.toDouble()
+    println("input b")
+    val b = readLine()!!.toDouble()
+    val cmp = a > b
+
+    println("inequality $a > $b: is $cmp")
+}
