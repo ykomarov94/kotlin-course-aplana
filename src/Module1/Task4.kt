42cfb28056ceb89961358cf4959971c9f9b3d67c
ykomarov94 <ykomarov94@gmail.com>
2019-10-06 14:57:34 +0300

    Задание 4: Приветствие пользователя

---
+++ src/Module1/Task4.kt
@@ -0,0 +1,8 @@
+package Module1
+
+fun main() {
+    println("Hi there! What can I call you?")
+    val userName = readLine()
+    println("Nice to meet you, $userName. And I am a minimalistic AI")
+    println("I wish that programmer wrote something else...")
+}
