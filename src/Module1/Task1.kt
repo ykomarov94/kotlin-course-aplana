abbc16b34c065378fad7cee3c1ed8c7a93043395
ykomarov94 <ykomarov94@gmail.com>
2019-10-06 14:34:01 +0300

    Задание 1: Переменные

---
+++ src/Module1/Task1.kt
@@ -0,0 +1,9 @@
+package Module1
+
+fun main(args: Array<String>) {
+    var undefinedVariable: Any? = null
+    undefinedVariable = 73 // the best number according to dr. Cooper
+    val implicitTypedVar = undefinedVariable / 2.5
+    // Последняя переменная имеет тип Double:
+    println(implicitTypedVar is Double)
+}
