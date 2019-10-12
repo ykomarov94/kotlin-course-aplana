4af4aa998c0186fc13e4be0319b55dc5a0e95abd
ykomarov94 <ykomarov94@gmail.com>
2019-10-12 13:23:04 +0300

    Задание 19: Авиакомпания 3. Наследование

---
+++ src/module4/Boeing747.kt
@@ -0,0 +1,7 @@
+package module4
+
+// В качестве примера параметров взят Boeing 747-400
+// https://www.airlines-inform.ru/commercial-aircraft/Boeing-747-400.html
+class Boeing747 : Aircraft(747, 14200.0,241140.0){
+    var MaxPassengers: Int = 660
+}
---
+++ src/module4/task19.kt
@@ -0,0 +1,7 @@
+package module4
+
+fun main(){
+    val boeing747 = Boeing747()
+    println(boeing747.FuelConsumption)
+    println(boeing747.MaxPassengers)
+}
